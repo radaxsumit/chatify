74201edf242623ebc855ad17f850aa0a0deb4e3e
Sumit Prajapati <amanprajapatirada@gmail.com>
2026-03-24 18:30:18 +0530

    The Rate Limiter is deployed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import {signup, login, logout, updateProfile} from '../controllers/auth.controller.js';
 import {protectRoute} from "../middleware/auth.middleware.js";
+import {arcjetProtection} from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-// Authentication routes
-router.post('/signup', signup);
-// Login and logout routes
-router.post('/login', login);
-// Logout and logout routes
-router.post('/logout', logout);
+// To test the ArcjetProtection working or not ✅
+// router.get('/test', arcjetProtection, (req, res) => {
+//     res.status(200).send({message: "Welcome Arcjet Test."});
+// });
 
-router.put('/update-profile', protectRoute, updateProfile);
+router.use(arcjetProtection);    // We don't have to add 'arcjetProtection' in every single route so.
+
+// Authentication routes
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
 
 // To check User is authenticated or not...
-router.get('/check', protectRoute, (req, res) => res.status(200).json(req.user));
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
