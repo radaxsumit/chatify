20a0347905a4767f2781be77e8168363f68fb51f
Sumit Prajapati <amanprajapatirada@gmail.com>
2026-03-26 18:04:52 +0530

    Frontend Setup Begin

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ChatPage = () => {
+    return (
+        <div>
+            <button className="btn btn-primary ">hey this is for test Purposes Only !!! </button>
+        </div>
+    )
+}
+export default ChatPage
